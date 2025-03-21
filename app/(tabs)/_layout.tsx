cd9034789f53fa4a317b211349c1e37cdc2d912f
emiresen <emiresen246@gmail.com>
2025-03-21 22:21:37 +0300

    initial Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Tabs } from 'expo-router';
+import { Text } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { fetchLogin } from '@/store/feature/AuthSlice';
+import { AppDispatch } from '@/store';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { getInsights, getMenstrationDays, getProfile } from '@/store/feature/ProfileSlice';
+import Dot from '@/components/beije/bottomtab/dot';
+import Calender from '@/components/beije/bottomtab/calender';
+import Analysis from '@/components/beije/bottomtab/analysis';
+import Guide from '@/components/beije/bottomtab/guide';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -16,42 +22,74 @@ function TabBarIcon(props: {
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const dispatch = useDispatch<AppDispatch>();
+
+  //Mocking login with provided data
+  useEffect(() => {
+    dispatch(fetchLogin({
+      "email": "salar@beije.co",
+      "password": "beijeApp"
+    }))
+      .then(() => {
+        dispatch(getProfile())
+        dispatch(getMenstrationDays())
+        dispatch(getInsights())
+      });
+  }, [dispatch])
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#F69C85",
+        tabBarInactiveTintColor: "#B1B1B1",
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {
+          backgroundColor: '#F7F7F7',
+          borderTopWidth: 0
+        },
       }}>
+
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          title: 'Döngü',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <Dot color={color} />,
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ fontSize: 10, color: focused ? '#343131' : '#B1B1B1' }}>Döngü</Text>
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="calender"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Takvim',
+          tabBarIcon: ({ color }) => <Calender color={color} />,
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ fontSize: 10, color: focused ? '#343131' : '#B1B1B1' }}>Takvim</Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="analysis"
+        options={{
+          title: 'Analiz',
+          tabBarIcon: ({ color }) => <Analysis color={color} />,
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ fontSize: 10, color: focused ? '#343131' : '#B1B1B1' }}>Analiz</Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="guide"
+        options={{
+          title: 'Rehber',
+          tabBarIcon: ({ color }) => <Guide color={color} />,
+          tabBarLabel: ({ focused }) => (
+            <Text style={{ fontSize: 10, color: focused ? '#343131' : '#B1B1B1' }}>Rehber</Text>
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,114 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, SafeAreaView, Pressable, FlatList } from 'react-native';
+import { useAppSelector } from '@/store';
+import { LinearGradient } from 'expo-linear-gradient';
+import BellIcon from '@/components/beije/ui-from-figma/BellIcon';
+import CycleTracker from '@/components/beije/CycleTracker';
+import TodaysHighlightsBottomSheet from '@/components/beije/TodaysHighlightsBottomSheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function CycleScreen() {
+  const profile = useAppSelector(state => state.profile)
+  const [isBottomSheetExpanded, setIsBottomSheetExpanded] = useState(false);
 
-export default function TabOneScreen() {
+  // Handle bottom sheet expansion state
+  const handleSheetExpand = (isExpanded: boolean) => {
+    setIsBottomSheetExpanded(isExpanded);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <LinearGradient
+        colors={['#FDECE7', '#FDECE7']}
+        style={styles.gradient}
+      >
+        <SafeAreaView style={styles.container}>
+          {/* Profile icon and notification icon could go here */}
+          <View style={styles.header}>
+            <View style={styles.profileIcon}>
+              <Text style={styles.profileText}>
+                {profile?.profile?.profileInfo.firstName[0].toUpperCase()}
+              </Text>
+            </View>
+            <View >
+              <Pressable
+                style={styles.notificationIcon}
+                onPress={() => console.log("Directing to Notifications. [Not requested]")}>
+                <BellIcon />
+              </Pressable>
+            </View>
+          </View>
+
+          {profile.menstrationDays && (
+            <CycleTracker
+              cycleData={profile.menstrationDays}
+              isExpanded={isBottomSheetExpanded}
+            />
+          )}
+
+          <TodaysHighlightsBottomSheet
+            insights={profile.insights ?? undefined}
+            onSheetExpand={handleSheetExpand}
+          />
+        </SafeAreaView>
+      </LinearGradient>
+    </GestureHandlerRootView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  gradient: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  profileIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#F06A47',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileText: {
+    color: 'white',
+    fontSize: 18,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  notificationIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
   },
+  cycleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+  dateText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: 'center',
+  }
 });
